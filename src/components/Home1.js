3135a5af5445f17a39fca99bd8172e1b7950d595
Lucas <104266998+lucas-twygz@users.noreply.github.com>
2024-10-01 16:45:06 -0300

    navbar v1
    Co-Authored-By: Natan Pucci <natan.puccib@outlook.com>
    Co-Authored-By: Sam06tanb <78392140+sam06tanb@users.noreply.github.com>

---
+++ src/components/Home1.js
@@ -0,0 +1,14 @@
+import styles from "./Home1.module.css";
+
+function Home1(){
+    return (
+        <>
+            <section>
+                <div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Home1;
